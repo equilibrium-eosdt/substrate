46bcbb4e5e58d0f04005bf3b41d3726c8b9ada73
Veniamin <benjamin@eosdt.com>
2021-03-12 15:50:19 +0300

    added eq ss58 prefix for release

--- primitives/core/src/crypto.rs
+++ primitives/core/src/crypto.rs
@@ -582,6 +582,8 @@ ss58_address_format!(
 		(65, "aventus", "Aventus Chain mainnet, standard account (*25519).")
 	CrustAccount =>
 		(66, "crust", "Crust Network, standard account (*25519).")
+	EquilibriumAccount =>
+		(67, "equilibrium", "Equilibrium Network, standard account (*25519).")
 	// Note: 48 and above are reserved.
 );
 
